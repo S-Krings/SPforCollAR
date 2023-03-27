a8801c680e6e3ade12ed28a528415f8535a521c2
S-Krings <sarah.krings@gmx.net>
2023-03-27 16:55:36 +0200

    Make ColourEnum not MonoBehaviour

--- SPforCollAR/Assets/Scripts/ColourEnum.cs
+++ SPforCollAR/Assets/Scripts/ColourEnum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ColourEnum : MonoBehaviour
+public class ColourEnum
 {
     public enum PlayerColorEnum
     {
