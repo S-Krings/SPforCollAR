64fb9f8567bfa940ce9503000df5f3e9359e1fbd
S-Krings <sarah.krings@gmx.net>
2023-04-12 00:01:20 +0200

    Include Rigidbody sync, add it to cube+sphere

--- SPforCollAR/Assets/Scripts/MovementUpdater.cs
+++ SPforCollAR/Assets/Scripts/MovementUpdater.cs
@@ -6,28 +6,45 @@ using Mirror;
 public class MovementUpdater : NetworkBehaviour
 {
 
-    public bool isManipulated = false;
+    [SerializeField] bool isManipulated = false;
+    [SerializeField] Rigidbody targetRigidbody;
     //public PlayerScript player;
 
-    /*private void Start()
+    private void Start()
     {
-        if (!isServer)
+        if (isServer)
         {
-            this.GetComponent<NetworkTransform>().enabled = false;
+            targetRigidbody.isKinematic = false;
         }
-    }*/
+    }
 
     void Update()
     {
         if (isManipulated)
         {
+            if (targetRigidbody != null) targetRigidbody.isKinematic = false;
             //Debug.Log("Update: Moving, transform x:" + this.gameObject.transform.position.x + " y " + this.gameObject.transform.position.y + " z " + this.gameObject.transform.position.z);
             CmdUpdatePosition(this.gameObject.transform.position, this.gameObject.transform.rotation, this.gameObject.transform.localScale, this.netIdentity);//, player);
+        }
+        else
+        {
+            if (targetRigidbody != null)
+            {
+                if (!isServer)
+                {
+                    targetRigidbody.isKinematic = true;
+                }
+                else
+                {
+                    RPCUpdateClientPos(this.gameObject.transform.position, this.gameObject.transform.rotation, this.gameObject.transform.localScale, this.netIdentity);//, player);
+                }
+            }
         }
     }
     public void runUpdates()
     {
         isManipulated = true;
+        if (!isServer) CmdToggleKinematic(true);
     }
 
     [Command(requiresAuthority = false)]
@@ -60,9 +77,42 @@ public class MovementUpdater : NetworkBehaviour
         }*/
     }
 
+    [Command(requiresAuthority = false)]
+    void CmdUpdateVelocity(Vector3 velocity, Vector3 angularVelocity, NetworkIdentity id)//, PlayerScript player)
+    {
+        //TODO: here, we can check if client is allowed to do the change!!!
+        GameObject g = id.gameObject;
+
+        //this.velocity = velocity;
+        targetRigidbody.velocity = velocity;
+
+        //this.angularVelocity = angularVelocity;
+        targetRigidbody.angularVelocity = angularVelocity;
+
+        RPCUpdateClientVelocity(velocity, angularVelocity, id);//, player);
+    }
+
+    [ClientRpc]
+    void RPCUpdateClientVelocity(Vector3 velocity, Vector3 angularVelocity, NetworkIdentity id)//, PlayerScript player)
+    {
+        GameObject g = id.gameObject;
+    }
+
     public void stopUpdates()
     {
         isManipulated = false;
+        if (!isServer) CmdToggleKinematic(false);
+
+        if (targetRigidbody != null)
+        {
+            CmdUpdateVelocity(targetRigidbody.velocity, targetRigidbody.angularVelocity, this.netIdentity);
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdToggleKinematic(bool rigidbodyKinematic)//, PlayerScript player)
+    {
+        if(targetRigidbody != null) targetRigidbody.isKinematic = rigidbodyKinematic;
     }
 
 }
