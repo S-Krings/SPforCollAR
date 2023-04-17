a8fa0ea7679ea17258d23cc6d29209d09a3c5d91
S-Krings <sarah.krings@gmx.net>
2023-04-17 13:41:44 +0200

    Create flat floor

--- SPforCollAR/Assets/Scripts/PlanePlacer.cs
+++ SPforCollAR/Assets/Scripts/PlanePlacer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlanePlacer : MonoBehaviour
 {
     [SerializeField] private bool isOnFloor;
+    [SerializeField] private int stillCounter;
     [SerializeField] Rigidbody planeRigidbody;
     private Vector3 lastPosition = new Vector3(0, 0, 0);
 
@@ -14,10 +15,18 @@ public class PlanePlacer : MonoBehaviour
         {
             if(System.Math.Round(planeRigidbody.position.y,3) == System.Math.Round(lastPosition.y, 3))
             {
-                isOnFloor = true;
-                planeRigidbody.gameObject.transform.localScale *= 100;
-                planeRigidbody.isKinematic = true;
-                planeRigidbody.gameObject.transform.up = Vector3.up;
+                stillCounter++;
+                if (stillCounter > 10)
+                {
+                    isOnFloor = true;
+                    planeRigidbody.gameObject.transform.localScale *= 100;
+                    planeRigidbody.isKinematic = true;
+                    planeRigidbody.gameObject.transform.up = Vector3.up;
+                }
+            }
+            else
+            {
+                stillCounter = 0;
             }
             lastPosition = planeRigidbody.position;
         }
