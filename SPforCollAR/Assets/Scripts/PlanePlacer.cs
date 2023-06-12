eaac12274ccb86a7d369afb252982271d32e2b4e
S-Krings <sarah.krings@gmx.net>
2023-06-12 08:13:30 +0200

    Revert "Create flat floor"
    This reverts commit a8fa0ea7679ea17258d23cc6d29209d09a3c5d91.

--- SPforCollAR/Assets/Scripts/PlanePlacer.cs
+++ SPforCollAR/Assets/Scripts/PlanePlacer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlanePlacer : MonoBehaviour
 {
     [SerializeField] private bool isOnFloor;
-    [SerializeField] private int stillCounter;
     [SerializeField] Rigidbody planeRigidbody;
     private Vector3 lastPosition = new Vector3(0, 0, 0);
 
@@ -15,18 +14,10 @@ public class PlanePlacer : MonoBehaviour
         {
             if(System.Math.Round(planeRigidbody.position.y,3) == System.Math.Round(lastPosition.y, 3))
             {
-                stillCounter++;
-                if (stillCounter > 10)
-                {
-                    isOnFloor = true;
-                    planeRigidbody.gameObject.transform.localScale *= 100;
-                    planeRigidbody.isKinematic = true;
-                    planeRigidbody.gameObject.transform.up = Vector3.up;
-                }
-            }
-            else
-            {
-                stillCounter = 0;
+                isOnFloor = true;
+                planeRigidbody.gameObject.transform.localScale *= 100;
+                planeRigidbody.isKinematic = true;
+                planeRigidbody.gameObject.transform.up = Vector3.up;
             }
             lastPosition = planeRigidbody.position;
         }
