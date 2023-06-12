e2a0a0da6622691107cc5cb828cd10d17384cef2
S-Krings <sarah.krings@gmx.net>
2023-06-13 01:26:21 +0200

    First draft of a user interface for toolkit

--- SPforCollAR/Assets/PrivacySecurityToolkit/UserInterface/Editor/SelectionWindow.cs
+++ SPforCollAR/Assets/PrivacySecurityToolkit/UserInterface/Editor/SelectionWindow.cs
@@ -24,6 +24,9 @@ public class SelectionWindow : EditorWindow
     [Tooltip("What happens in one application instance")] [SerializeField] bool InApplication;
     [Tooltip("What happens in the real world")] [SerializeField] bool Physical;
 
+    private bool PrivacyShield;
+    private bool PermissionMenu;
+    private bool Dissolve;
 
     [MenuItem("PrivacySecurityToolbox/Configuration Window")]
     public static void ShowWindow()
@@ -86,15 +89,92 @@ public class SelectionWindow : EditorWindow
             "Distraction " + Distraction+ "\n" +
             "Illusion " + Illusion+ "\n" +
             "UnwantedContentPlacement " + UnwantedContentPlacement);
+            AddFeatures();
         }
-    }
-
-    private void FindFeatures()
-    {
-        if (Users && OtherUsers && SituationalInformationDisclosure)
+        if (PermissionMenu)
         {
-
+            EditorGUILayout.HelpBox("Permission Manager Button Added - What to do now?\n" +
+            "  - Add the PermissionInterestManagement component to the GameObject your NetworkManager is on\n" +
+            "  - Add the PermissionInterestManagementOverride component to objects that should always appear\n" +
+            "  - Use the PermissionManager.singleton in your code:\n" +
+            "     - Use AddStandardPermissions when spawining new objects\n" +
+            "     - Use checkPermission when allowing actions that only authorized users should be able to do",
+            MessageType.None);
+        }
+        if (PrivacyShield)
+        {
+            EditorGUILayout.HelpBox("Protection Shield Button Added - What to do now?\n" +
+            "Nothing, you're done!",
+            MessageType.None);
+        }
+        if (Dissolve)
+        {
+            EditorGUILayout.HelpBox("Dissolve Button Added - What to do now?\n" +
+            "  - Add \"Dissolve\" tag to objects that should dissolve\n" +
+            "  - Do not forget the prefabs that can be spawned",
+            MessageType.None);
         }
     }
 
+    private void AddFeatures()
+    {
+        GameObject menu = GameObject.Find("HandProtectionMenu");
+        if (menu == null)
+        {
+            Object prefab = AssetDatabase.LoadAssetAtPath("Assets/PrivacySecurityToolkit/SharedAssets/Resources/HandProtectionMenu.prefab", typeof(GameObject));
+            if (prefab == null)
+            {
+                Debug.Log("HandProtectionMenu missing");
+                return;
+            }
+            menu = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            Undo.RegisterCreatedObjectUndo(menu, "Create " + menu.name);
+        }
+        GameObject permissionManagerButton = null, shieldButton = null, dissolveButton = null;
+        foreach (Transform child in menu.transform.GetChild(0).GetChild(0))
+        {
+            Debug.Log(child.name);
+            if(child.name == "PermissionButton")
+            {
+                permissionManagerButton = child.gameObject;
+            }
+            if(child.name == "ShieldButton")
+            {
+                shieldButton = child.gameObject;
+            }
+            if(child.name == "ClearViewButton")
+            {
+                dissolveButton = child.gameObject;
+            }
+        }
+        Debug.Log("Permission " + permissionManagerButton + " dheld: " + shieldButton + " dissolve: " + dissolveButton);
+
+        if (!((Users||UsedData)&&OtherUsers && (StaticInformationDisclosure||SituationalInformationDisclosure)))
+        {
+            permissionManagerButton.SetActive(false);
+            PermissionMenu = false;
+        }
+        else
+        {
+            PermissionMenu = true;
+        }
+        if(!(Users && OtherUsers && SituationalInformationDisclosure))
+        {
+            shieldButton.SetActive(false);
+            PrivacyShield = false;
+        }
+        else
+        {
+            PrivacyShield = true;
+        }
+        if (!(Users && (OtherUsers||OutsideAttackers) && Occlusion))
+        {
+            dissolveButton.SetActive(false);
+            Dissolve = false;
+        }
+        else
+        {
+            Dissolve = true;
+        }
+    }
 }
